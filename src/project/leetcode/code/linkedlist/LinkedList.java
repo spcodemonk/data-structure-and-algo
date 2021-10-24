babec9f18bccfe02eb0b88cb38a2d602a42a85cf
sharadpawar01 <sharadpawar01@gmail.com>
2021-10-24 10:45:17 +0530

    list palindrome

--- src/project/leetcode/code/linkedlist/LinkedList.java
+++ src/project/leetcode/code/linkedlist/LinkedList.java
@@ -214,12 +214,11 @@ public class LinkedList {
 	}
 
 	// Merge 2 sorted lists
-
 	public static Node MergeSortedLists(LinkedList list, LinkedList list2) {
 
 		Node p = list.head;
 		Node q = list2.head;
-		
+
 		Node sortedHead = null;
 		Node sorted = null;
 		if (p.data < q.data) {
@@ -229,8 +228,8 @@ public class LinkedList {
 			sorted = q;
 			q = q.next;
 		}
-		
-		sortedHead=sorted;
+
+		sortedHead = sorted;
 
 		while (p != null && q != null) {
 
@@ -245,47 +244,161 @@ public class LinkedList {
 				q = q.next;
 			}
 		}
-		
-		if(p==null) {
-			sorted.next=q;
+
+		if (p == null) {
+			sorted.next = q;
 		}
-		
-		if(q==null){
-			sorted.next=p;
+
+		if (q == null) {
+			sorted.next = p;
 		}
 		return sortedHead;
 
+	}
+
+// Find sum of all elements in linkedList
+	public void sumOflinkedList(LinkedList list) {
+		int sum = findSum(list.head);
+		System.out.println("LinkedList sum is : " + sum);
+	}
+
+	// Recursive find sum of all elements
+	public int findSum(Node node) {
+
+		if (node == null)
+			return 0;
+
+		return node.data + findSum(node.next);
+	}
+
+	// recursive element search in list
+
+	public void recursiveSearch(int value) {
+
+		Node temp = head;
+
+		Node n = recursiveSearch1(temp, value);
+
+		if (n == null) {
+			System.out.println("Element not found");
+		} else {
+			System.out.println("Element found : " + n.data);
+		}
+	}
+
+	public Node recursiveSearch1(Node node, int value) {
+
+		if (node == null)
+			return null;
+
+		if (node.data == value)
+			return node;
+
+		return recursiveSearch1(node.next, value);
+	}
+
+	// Check if list is palindrome or not
+
+	public boolean palindromeCheck() {
+		
+		
+		Node temp = head;
+
+		//1. split the list
+		Node p = temp;
+		Node q = temp;
+
+		Node secondListStart = null;
+		while (true) {
+			p = p.next.next;
+
+			if (p.next == null) {
+				secondListStart = q.next.next;
+				break;
+			}
+
+			if (p == null) {
+				secondListStart = q.next;
+				break;
+			}
+			q = q.next;
+
+		}
+
+		q.next = null;
+		
+		//2. reverse second list
+		
+		Node currentNode = secondListStart, nextNode = secondListStart;
+
+		Node prev = null;
+
+		while (nextNode.next != null) {
+			nextNode = nextNode.next;
+
+			currentNode.next = prev;
+
+			prev = currentNode;
+
+			currentNode = nextNode;
+
+		}
+		currentNode.next = prev;
+		
+		//3. Now start comparing element of both the list from start
+		boolean isPalindrome=true;
+		temp = head;
+		Node secondReversed = currentNode;
+		while(temp!=null) {
+			
+			if(!(temp.data == secondReversed.data)) {
+				isPalindrome=false;
+			}
+			
+			temp=temp.next;
+			secondReversed=secondReversed.next;
+			
+			
+		}
+
+		return isPalindrome;
 	}
 
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
 
-		list.insertAtHead(12);
-		list.insertAtHead(8);
-		list.insertAtHead(5);
+		list.insertAtHead(1);
+		list.insertAtHead(4);
 		list.insertAtHead(3);
+		list.insertAtHead(2);
 		list.insertAtHead(1);
 
-		LinkedList list2 = new LinkedList();
-
-		list2.insertAtHead(7);
-		list2.insertAtHead(6);
-		list2.insertAtHead(5);
-		list2.insertAtHead(4);
-		list2.insertAtHead(2);
+//		LinkedList list2 = new LinkedList();
+//
+//		list2.insertAtHead(7);
+//		list2.insertAtHead(6);
+//		list2.insertAtHead(5);
+//		list2.insertAtHead(4);
+//		list2.insertAtHead(2);
 
 		System.out.println("-----------------Inupt-----------------------");
 		list.print();
 
+		System.out.println();
+		boolean isPalindrome =  list.palindromeCheck();
 
-		list2.print();
-		
-		System.out.println("-----------------Output----------------------");
-		
-		Node newList = MergeSortedLists(list,list2);
-//		Node newList = list.reverseLinkedList();
-		list.print(newList);
+		System.out.println("isPalindrome : "+isPalindrome);
+
+//		list2.print();
+//		
+//		System.out.println("-----------------Output----------------------");
+//		
+//		Node newList = MergeSortedLists(list,list2);
+////		Node newList = list.reverseLinkedList();
+//		list.print(newList);
+
+//		list.recursiveSearch(8);
 
 	}
 
