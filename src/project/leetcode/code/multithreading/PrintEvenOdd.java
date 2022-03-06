da64891065f0e58fe40a2ea1c389b1ec60b188be
sharadpawar01 <sharadpawar01@gmail.com>
2022-03-06 16:48:35 +0530

    multitherading commit

--- src/project/leetcode/code/multithreading/PrintEvenOdd.java
+++ src/project/leetcode/code/multithreading/PrintEvenOdd.java
@@ -1,34 +1,43 @@
 package project.leetcode.code.multithreading;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PrintEvenOdd {
 
 	public static void main(String[] args){
 
 		AtomicBoolean EvenOddFlag = new AtomicBoolean(true);
-
+		
+		AtomicInteger count = new AtomicInteger(0);
 		Runnable even = () -> {
 
-			int i=0;
-			while(i<=10) {
-				
-				if(EvenOddFlag.get()==true) {
-					System.out.println("Even = "+i);
-					i++;
+			while(count.get()<=10) {
+				if(EvenOddFlag.get()==true ) {
+					System.out.println("Even = "+count.getAndIncrement());
+					
 					EvenOddFlag.set(false);
 					sleepMyThread();
-				}else {
-					System.out.println("Odd = "+i);
-					i++;
+				}
+			}
+			
+		};
+		
+		Runnable odd = () -> {
+
+			while(count.get()<=10) {
+				if(EvenOddFlag.get()==false) {
+					System.out.println("Odd = "+count.getAndIncrement());
+					
 					EvenOddFlag.set(true);
 					sleepMyThread();
 				}
 			}
+		
 		};
 		
-		new Thread(even).start();
-
+		new Thread(even).start();	
+		new Thread(odd).start();	
 	}
 	
 	public static void sleepMyThread() {
